8ae1c94efe335139a0d0d41c5b2ec62d67de4cbb
Мардарь Ольга (Mardar Olga) <omardar@beua.com>
2016-11-16 11:51:25 +0200

    pokedex full

--- projects/pokedex/views/pokemon-lg/pokemon-lg.js
+++ projects/pokedex/views/pokemon-lg/pokemon-lg.js
@@ -9,16 +9,27 @@ var Pokedex;
                 var _this = this;
                 this.id = $stateParams.pokemonId;
                 this.http = $http;
-                var url = "http://pokeapi.co/api/v1/pokemon/" + this.id + ".png";
+                var url = "http://pokeapi.co/api/v1/pokemon/" + this.id;
                 this.listProperty = [];
                 this.loading = document.querySelector(".loading");
                 this.loading.style.display = "block";
+                this.view = document.querySelector(".view-pokemon");
+                this.view.style.display = "none";
                 this.http.get(url).success(function (data) { return _this.allInfo(data); });
             }
             PokemonView.prototype.allInfo = function (data) {
                 this.pokemonName = data.name;
                 this.pokemonImage = "http://pokeapi.co/media/img/" + data.pkdx_id + ".png";
-                this.listProperty.push({ property: "Type", value: data["types"] });
+                var listOfTypes = "";
+                for (var i = 0, max = data["types"].length; i < max; i++) {
+                    if (listOfTypes == "") {
+                        listOfTypes = data.types[i].name;
+                    }
+                    else {
+                        listOfTypes += "," + data.types[i].name;
+                    }
+                }
+                this.listProperty.push({ property: "Type", value: listOfTypes });
                 this.listProperty.push({ property: "Attack", value: data["attack"] });
                 this.listProperty.push({ property: "Defense", value: data["defense"] });
                 this.listProperty.push({ property: "HP", value: data["hp"] });
@@ -28,6 +39,28 @@ var Pokedex;
                 this.listProperty.push({ property: "Weight", value: data["weight"] });
                 this.listProperty.push({ property: "Total moves", value: data["total"] });
                 this.loading.style.display = "none";
+                this.view.style.display = "block";
+                this.centerBlock();
+            };
+            PokemonView.prototype.centerBlock = function () {
+                var block = document.querySelector(".view-pokemon");
+                if (document.documentElement.clientWidth > 990) {
+                    var container = document.querySelector(".container");
+                    var coord = container.getBoundingClientRect();
+                    block.style.right = (coord.left + 60) + "px";
+                    block.style.transform = "translateY(-50%)";
+                    block.style.width = "30%";
+                }
+                else {
+                    block.style.right = "50%";
+                    block.style.transform = "translate(50%, -50%)";
+                    block.style.width = "80%";
+                }
+                block.style.display = "block";
+            };
+            PokemonView.prototype.closeWindow = function () {
+                var block = document.querySelector(".view-pokemon");
+                block.style.display = "none";
             };
             return PokemonView;
         }());
