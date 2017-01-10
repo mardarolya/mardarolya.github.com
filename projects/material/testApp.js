6ac41ec0a7173fca6534ab64235308e6e8274b5a
Мардарь Ольга (Mardar Olga) <omardar@beua.com>
2017-01-10 16:56:11 +0200

    start working with API

--- projects/material/testApp.js
+++ projects/material/testApp.js
@@ -1,22 +1,19 @@
 var testApp = angular.module("TestApp", ["ngMaterial"])
-	.run(function(){
-		console.log("I am ready");
-	});
+  .controller("taskController", ["$scope", "$http", function ($scope, $http) {
 
-testApp.controller("taskController", function ($scope, $mdBottomSheet) {
-    $scope.projects = [{name:"Private", tasks: 8}, 
-                       {name: "Decode", tasks: 25},
-                       {name: "Family", tasks: 3},
-                       {name: "Cookle", tasks: 13}];
-    $scope.currentUser = "Trevor Reyes";
-    $scope.tasks = [{date: "Today", names: [{name: "Create a company", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}]}, 
-                    {date: "Tomorrow",  names: [{name: "Call in barber shop", description: ""}]},
-                    {date: "Friday (09.06.2016)",  names: [{name: "Earn a lot of money", description: ""}, 
-                                                   {name: "Go to the shop", description: ""}]},
-                    {date: "Suturday (10.06.2016)", names: [{name: "Buy gifts", description: ""}]}, 
-                    {date: "Sunday (11.06.2016)",  names: [{name: "Brush teeth", description: ""}]},  
-                    {date: "Monday (12.06.2016)",  names: [{name: "Buy a plane tickets", description: ""}, 
-                                                   {name: "Fly away to the Thailand", description: ""}]} ];
+    // $scope.projects = [{name:"Private", tasks: 8}, 
+    //                    {name: "Decode", tasks: 25},
+    //                    {name: "Family", tasks: 3},
+    //                    {name: "Cookle", tasks: 13}];
+    // $scope.currentUser = "Trevor Reyes";
+    // $scope.tasks = [{date: "Today", names: [{name: "Create a company", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}]}, 
+    //                 {date: "Tomorrow",  names: [{name: "Call in barber shop", description: ""}]},
+    //                 {date: "Friday (09.06.2016)",  names: [{name: "Earn a lot of money", description: ""}, 
+    //                                                {name: "Go to the shop", description: ""}]},
+    //                 {date: "Suturday (10.06.2016)", names: [{name: "Buy gifts", description: ""}]}, 
+    //                 {date: "Sunday (11.06.2016)",  names: [{name: "Brush teeth", description: ""}]},  
+    //                 {date: "Monday (12.06.2016)",  names: [{name: "Buy a plane tickets", description: ""}, 
+    //                                                {name: "Fly away to the Thailand", description: ""}]} ];
 
     $scope.showRightPanel = false; 
     $scope.isOpenTask = false; 
@@ -31,8 +28,11 @@ testApp.controller("taskController", function ($scope, $mdBottomSheet) {
     this.newTaskName = "";
     this.newTaskDescription = "";
 
+    var way = "https://api-test-task.decodeapps.io";
+    var currentSession = "";
+
     this.showPanel = function(page, head, body) {
-	  $scope.panelHeader = head;
+	    $scope.panelHeader = head;
       $scope.panelBody = body;
       this.pageName = page;
       if (page == 'taskView') {
@@ -75,4 +75,106 @@ testApp.controller("taskController", function ($scope, $mdBottomSheet) {
     	}
     	this.closePanel();
     }
-});	
+
+    this.getTasks = function(id) {
+      console.log(currentSession);
+      // console.log('getTask');
+       $http.get(way + '/tasks?session=' + currentSession + '&project_id=' + id + '&paging_size=20&paging_offset=10')
+            .then(function (data) {
+               console.log(data);
+            }, function (error) {
+               console.log(error);
+            });  
+    }
+
+    function letsGo(){
+      currentSession = getCookie("mySession");      
+      if (currentSession) {
+        $http.get(way + '/session?session='+currentSession)
+            .then(function (data) {
+
+               getInfo(data.data.session);
+            }, function (error) {
+               console.log(error);
+            });   
+      } else {
+        $http.post(way + '/signup', {"New item":"Tomas New"})
+          .then(function (data) {
+                  setCookie("mySession", data.data.session);
+                  getInfo(data.data.session);
+                },
+                function (error){
+                  console.log(error);
+                });
+
+      }
+    }
+
+    function getInfo(session) {
+      currentSession = session; 
+      $http.get(way + '/account?session='+session)
+                  .then(
+                    function (data) {
+                      $scope.currentUser = data.data.Account.username;
+                      var photo = document.querySelector(".with-frame img");
+                      photo.src = data.data.Account.image_url;
+                      
+                      $http.get(way + '/projects?session='+session)
+                           .then(
+                              function (data) {
+                                $scope.projects = data.data.projects;
+                                $scope.loading = false;
+                              },                     
+                              function (error) {
+                                console.log(error);
+                              });  
+                    },
+                    function (error) {
+                      console.log(error);
+                    });      
+    }
+
+    function getCookie(name) {
+      var matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+      ));
+      return matches ? decodeURIComponent(matches[1]) : undefined;
+    }
+
+    function setCookie(name, value, options) {
+      options = options || {};
+
+      var expires = options.expires;
+
+      if (typeof expires == "number" && expires) {
+        var d = new Date();
+        d.setTime(d.getTime() + expires * 1000);
+        expires = options.expires = d;
+      }
+      if (expires && expires.toUTCString) {
+        options.expires = expires.toUTCString();
+      }
+
+      value = encodeURIComponent(value);
+
+      var updatedCookie = name + "=" + value;
+
+      for (var propName in options) {
+        updatedCookie += "; " + propName;
+        var propValue = options[propName];
+        if (propValue !== true) {
+          updatedCookie += "=" + propValue;
+        }
+      }
+
+      document.cookie = updatedCookie;
+    }
+
+    angular.element(document).ready(function () {
+      $scope.loading = true;
+      letsGo();
+    });
+}])
+  .run(function(){
+    console.log("I am ready");        
+  })
